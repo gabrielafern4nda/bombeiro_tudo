6b7e79589114af3a84b26d28de4ab63505e139f6
gabrielafern4nda <gabriela_f_borges@estudante.sesisenai.org.br>
2023-11-14 10:35:56 -0300

    porfavor

--- vitima_era/inserir.php
+++ vitima_era/inserir.php
@@ -1,40 +1,33 @@
 <?php
 include("conecta.php");
 
-// Verifica se o formulário foi submetido
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recebe os valores do formulário
-    $ciclista = $_POST["Ciclista"];
-    $condutorMoto = $_POST["CondutorMoto"];
-    $condutorCarro = $_POST["CondutorCarro"];
-    $clinico = $_POST["Clinico"];
-    $gestante = $_POST["Gestante"];
-    $trauma = $_POST["Trauma"];
-    $passageiroBancoFrente = $_POST["PassageiroBancoFrente"];
-    $passageiroBancoTras = $_POST["PassageiroBancoTras"];
-    $passageiroMoto = $_POST["PassageiroMoto"];
-    $pedestre = $_POST["Pedestre"];
-    // Adicione as demais variáveis conforme as colunas no banco
+$ciclista = isset($_POST["ciclista"]) ? $_POST["ciclista"] : 0;
+$condutorMoto = isset($_POST["condutorMoto"]) ? $_POST["condutorMoto"] : 0;
+$condutorCarro = isset($_POST["condutorCarro"]) ? $_POST["condutorCarro"] : 0;
+$clinico = isset($_POST["clinico"]) ? $_POST["clinico"] : 0;
+$gestante = isset($_POST["gestante"]) ? $_POST["gestante"] : 0;
+$trauma = isset($_POST["trauma"]) ? $_POST["trauma"] : 0;
+$passageiroBancoFrente = isset($_POST["passageiroBancoFrente"]) ? $_POST["passageiroBancoFrente"] : 0;
+$passageiroBancoTras = isset($_POST["passageiroBancoTras"]) ? $_POST["passageiroBancoTras"] : 0;
+$passageiroMoto = isset($_POST["passageiroMoto"]) ? $_POST["passageiroMoto"] : 0;
+$pedestre = isset($_POST["pedestre"]) ? $_POST["pedestre"] : 0;
 
-    // Prepara a consulta SQL
-    $comando = $pdo->prepare("INSERT INTO vitima_era (Ciclista, CondutorMoto, CondutorCarro, Clinico, Gestante, Trauma, PassageiroBancoFrente, PassageiroBancoTras, PassageiroMoto, Pedestre) VALUES (:ciclista, :condutorMoto, :condutorCarro, :clinico, :gestante, :trauma, :passageiroBancoFrente, :passageiroBancoTras, :passageiroMoto, :pedestre  )");
-    // Adicione os demais campos e valores na consulta conforme as colunas no banco
+$comando = $pdo->prepare("INSERT INTO vitima_era 
+    (Ciclista, CondutorMoto, CondutorCarro, Clinico, Gestante, Trauma, PassageiroBancoFrente, PassageiroBancoTras, PassageiroMoto, Pedestre)
+    VALUES (:ciclista, :condutorMoto, :condutorCarro, :clinico, :gestante, :trauma, :passageiroBancoFrente, :passageiroBancoTras, :passageiroMoto, :pedestre)");
 
-    // Executa a consulta
-    $resultado = $comando->execute(array(":ciclista" => $ciclista, ":condutorMoto" => $condutorMoto, ":condutorCarro" => $condutorCarro, ":clinico" => $clinico, ":gestante" => $gestante, ":trauma" =>  $trauma, ":passageiroBancoFrente" => $passageiroBancoFrente, ":passageiroBancoTras" =>  $passageiroBancoTras, ":passageiroMoto" => $passageiroMoto, ":pedestre" => $pedestre ));
-    // Adicione os demais campos e valores na execução conforme as colunas no banco
+$resultado = $comando->execute(array(
+    ":ciclista" => $ciclista,
+    ":condutorMoto" => $condutorMoto,
+    ":condutorCarro" => $condutorCarro,
+    ":clinico" => $clinico,
+    ":gestante" => $gestante,
+    ":trauma" => $trauma,
+    ":passageiroBancoFrente" => $passageiroBancoFrente,
+    ":passageiroBancoTras" => $passageiroBancoTras,
+    ":passageiroMoto" => $passageiroMoto,
+    ":pedestre" => $pedestre
+));
 
-    // Verifica se a inserção foi bem sucedida
-    if ($resultado) {
-        echo json_encode(array("resultado" => 1));
-    } else {
-        echo json_encode(array("resultado" => 0));
-    }
-} else {
-    echo json_encode(array("resultado" => 0));
-}
-
-$pdo->commit();
-
-
+echo json_encode(array("resultado" => $resultado ? 1 : 0));
 ?>
